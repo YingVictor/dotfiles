35c0e0dbec0ed8a3c2c2f1bcaaf3de5a95699804
Victor A. Ying <victory@csail.mit.edu>
2016-09-13 19:18:26 -0400

    Rename setup script backup directory.

--- .dotfiles-setup.sh
+++ .dotfiles-setup.sh
@@ -2,6 +2,6 @@ alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 git clone --bare git@github.com:YingVictor/dotfiles.git $HOME/.dotfiles
 mkdir $HOME/.config-backup && \
     dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
-    xargs -I{} mv {} $HOME/.config-backup/{}
+    xargs -I{} mv {} $HOME/.dotfiles-backup/{}
 dotfiles checkout
 dotfiles config --local status.showUntrackedFiles no
