3f4d86150aac9ff789d01241c17884e6c90fd94e
Victor A. Ying <victory@csail.mit.edu>
2016-09-19 15:07:38 -0400

    Add error check to git clone in setup.

--- .dotfiles-setup.sh
+++ .dotfiles-setup.sh
@@ -1,5 +1,10 @@
 git clone --bare git@github.com:YingVictor/dotfiles.git $HOME/.dotfiles
+if [ $? != 0 ]; then
+    return 1
+fi
+
 alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
 dotfiles checkout
 if [ $? == 0 ]; then
     echo "Checked out dotfiles."
