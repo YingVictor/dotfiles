981e1ab9712f713b298716e7cd00d16d042914b6
Victor A. Ying <victory@csail.mit.edu>
2016-09-28 15:30:25 -0400

    Allow setup.sh to handle subdirectories.

--- .setup.sh
+++ .setup.sh
@@ -5,6 +5,14 @@ fi
 
 alias dotfiles='git --git-dir=$HOME/.dotfiles/.git --work-tree=$HOME'
 
+function mvp ()
+{
+  dir="$2"
+  last_char="${dir: -1}"
+  [ "$last_char" != "/" ] && dir="$(dirname "$2")"
+  [ -d "$dir" ] || mkdir -p "$dir" && mv "$@"
+}
+
 dotfiles checkout
 if [ $? == 0 ]; then
     echo "Checked out dotfiles."
@@ -13,6 +21,6 @@ else
     BACKUP_DIR=$HOME/.backup
     mkdir -p $BACKUP_DIR && \
         dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
-        xargs -I{} mv {} $BACKUP_DIR/{}
+        xargs -I{} mvp {} $BACKUP_DIR/{}
     dotfiles checkout
 fi
